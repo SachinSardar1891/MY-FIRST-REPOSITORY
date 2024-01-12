c04b74012b9c8d298a6b5ddb8872a8a1cdeb7295
SachinSardar1891 <156399288+SachinSardar1891@users.noreply.github.com>
2024-01-11 21:32:17 -0800

    my simple java program

---
+++ firstprogram.java
@@ -0,0 +1,12 @@
+import.java.util.*;
+public class demo
+{
+public static void main(String args[])
+{
+String name;
+Scanner sc=new Scanner(System.ih)
+System.out.println("Enter youre name");
+name=sc.next();
+System.out.println(name);
+}
+}
